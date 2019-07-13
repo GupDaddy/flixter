499da14398cd5a3874aff771dd5ae2c8dbfdb0bc
Chris Guthrie <guthricb@gmail.com>
2019-07-13 20:18:42 +0000

    updated flixter-master

--- Image_Blur_3/pixel_blur_3.rb
+++ Image_Blur_3/pixel_blur_3.rb
@@ -5,40 +5,66 @@ class Image
 	end
 
 	def find_ones
-		# image_blur = @Images
 		ones = []
+		total_rows = @Images.length - 1
 		@Images.each_with_index do |row,row_num|
+			total_cols = row.length - 1
 			row.each_with_index do |item,col_num|
 				if item == 1
-					ones_sub = []
-					ones_sub.push(row_num,col_num)
-					ones.push(ones_sub)
+					x = y = @Distance
+					# y = @Distance
+					(0..x).each do |x|
+						(0..y).each do |y|
+							if col_num + y <= total_cols
+								ones_sub = []
+								ones_sub.push(row_num,col_num + y)
+								ones.push(ones_sub)
+							end
+							if col_num - y >= 0
+								ones_sub = []
+								ones_sub.push(row_num,col_num - y)
+								ones.push(ones_sub)
+							end
+							if row_num + x <= total_rows && col_num + y <= total_cols
+								ones_sub = []
+								ones_sub.push(row_num + x,col_num + y)
+								ones.push(ones_sub)
+							end
+							if row_num - x >= 0 && col_num - y >= 0
+								ones_sub = []
+								ones_sub.push(row_num - x,col_num - y)
+								ones.push(ones_sub)
+							end
+							if row_num + x <= total_rows && col_num - y >= 0
+								ones_sub = []
+								ones_sub.push(row_num + x,col_num - y)
+								ones.push(ones_sub)
+							end
+							if row_num - x >= 0 && col_num + y <= total_cols
+								ones_sub = []
+								ones_sub.push(row_num - x,col_num + y)
+								ones.push(ones_sub)
+							end
+						end
+						y -= 1
+					end
 				end
 			end
 		end
 		ones
 	end
 
+	def blur(distance)
+		@Distance =  distance
+	end
+
 	def blur_image
 		ones = find_ones
-		total_rows = @Images.length - 1
 		@Images.each_with_index do |row,row_num|
-			total_cols = row.length - 1
 			row.each_with_index do |item,col_num|
 				ones.each do |found_row,found_col|
 					if row_num == found_row && col_num == found_col
-						if row_num-1 >= 0
-							@Images[row_num-1][col_num] = 1
-						end
-						if row_num < total_rows
-							@Images[row_num+1][col_num] = 1
-						end
-						if col_num >= 1
-							@Images[row_num][col_num-1] = 1
-						end
-						if col_num < total_cols
-							@Images[row_num][col_num+1] = 1
-						end
+						@Images[found_row][found_col] = 1
 					end
 				end
 			end
@@ -53,45 +79,20 @@ class Image
 end
 
 image = Image.new([
-  [0, 0, 0, 0],
-  [0, 0, 0, 0],
-  [0, 0, 0, 0],
-  [0, 1, 0, 0],
-  [0, 0, 0, 0],
-  [0, 0, 0, 0]
+[0, 0, 0, 0, 0, 0, 0, 0, 0],
+[0, 0, 0, 0, 0, 0, 0, 0, 0],
+[0, 0, 0, 0, 0, 0, 0, 0, 0],
+[0, 0, 0, 0, 0, 0, 0, 0, 0],
+[0, 0, 0, 1, 0, 0, 0, 0, 0],
+[0, 0, 0, 0, 0, 0, 0, 0, 0],
+[0, 0, 0, 0, 0, 0, 0, 0, 0],
+[0, 0, 0, 0, 0, 0, 0, 0, 0],
+[0, 0, 0, 0, 0, 0, 0, 0, 0],
+[0, 0, 0, 0, 0, 0, 0, 0, 0],
+[0, 0, 0, 0, 0, 0, 0, 0, 0],
+[0, 0, 0, 0, 0, 0, 0, 0, 1],
 ])
-# image.output_image
-image.blur_image
-puts "One Pixel Transformation"
-puts ''
-image.output_image
 
-image = Image.new([
-  [0, 0, 0, 0],
-  [0, 0, 1, 0],
-  [0, 0, 0, 0],
-  [0, 1, 0, 0],
-  [0, 0, 0, 0],
-  [0, 0, 0, 0]
-])
-# image.output_image
+image.blur(3)
 image.blur_image
-puts ''
-puts "Two Pixel Transformation"
-puts ''
-image.output_image
-
-image = Image.new([
-  [0, 0, 0, 0],
-  [0, 0, 0, 0],
-  [0, 0, 0, 0],
-  [0, 0, 0, 0],
-  [0, 0, 0, 0],
-  [0, 0, 1, 0]
-])
-# image.output_image
-image.blur_image
-puts ''
-puts "Pixel Edge Transformation"
-puts ''
 image.output_image
